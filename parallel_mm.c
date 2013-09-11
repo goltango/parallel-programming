33348768da730e261b47f3e3bdbe86528ebd4f63
goltango <goltango@gmail.com>
2013-09-11 15:32:33 -0300

    Se agrega uso de argc y argv para seteo de tam. de buffer, cantidad de threads, y chunk. Se corrige el idioma de algunos comentarios del codigo.

--- parallel_mm.c
+++ parallel_mm.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #define BUFFSIZE 1000
+#define NUMTHREADS 4
 
 /* Time in seconds from some point in the past */
 double dwalltime();
@@ -10,35 +11,46 @@ int main(int argc, char *argv[])
 	double **a;
 	double **b;
 	double **c;
-	int nrows = BUFFSIZE,ncolumns = BUFFSIZE;
 	int i,j,k;
-	int	tid, nthreads, chunk;
+	int	tid, nthreads, chunk, num_threads, buffSize;
 	double timetick;
 
-	chunk = 10;
-	//reserva de memoria de a
-	a = malloc(nrows * sizeof(double *));
-	for(i = 0; i < nrows; i++)
+    if (argc != 4)
+    {
+        printf("\nUsage: parallel_mm [buffSize] [num_threads] [chunk]\n");
+        return 0;
+    }
+
+	buffSize = atoi(argv[1]);;
+	num_threads = atoi(argv[2]);
+	chunk = atoi(argv[3]);
+
+	/*** alloc memory for a matrix ***/
+	a = malloc(buffSize * sizeof(double *));
+	for(i = 0; i < buffSize; i++)
 	{
-		a[i] = malloc(ncolumns * sizeof(double));
+		a[i] = malloc(buffSize * sizeof(double));
 	}
-	//reserva de memoria de b
-	b = malloc(nrows * sizeof(double *));
-	for(i = 0; i < nrows; i++)
+	/*** alloc memory for b matrix ***/
+	b = malloc(buffSize * sizeof(double *));
+	for(i = 0; i < buffSize; i++)
 	{
-		b[i] = malloc(ncolumns * sizeof(double));
+		b[i] = malloc(buffSize * sizeof(double));
 	}
-	//reserva de memoria de c
-	c = malloc(nrows * sizeof(double *));
-	for(i = 0; i < nrows; i++)
+	/*** alloc memory for c matrix ***/
+	c = malloc(buffSize * sizeof(double *));
+	for(i = 0; i < buffSize; i++)
 	{
-		c[i] = malloc(ncolumns * sizeof(double));
+		c[i] = malloc(buffSize * sizeof(double));
 	}
-	/*** Marca de tiempo antes de seccion paralela ***/
+	/*** Set threads quantity ***/
+	omp_set_num_threads(num_threads);
+
+	/*** Set timetick before parallel execution ***/
     timetick = dwalltime();
 
 	/*** Spawn a parallel region explicitly scoping all variables ***/
-	#pragma omp parallel shared(a,b,c,nthreads,chunk) private(tid,i,j,k)
+	#pragma omp parallel shared(a,b,c,nthreads,chunk,buffSize) private(tid,i,j,k)
 	{
 		tid = omp_get_thread_num();
 		if (tid == 0)
@@ -49,27 +61,27 @@ int main(int argc, char *argv[])
         	}
 		/*** Initialize matrices ***/
 		#pragma omp for schedule (static, chunk)
-		for (i=0; i<BUFFSIZE; i++)
-			for (j=0; j<BUFFSIZE; j++)
+		for (i=0; i<buffSize; i++)
+			for (j=0; j<buffSize; j++)
 				a[i][j]= 1.0;
 		#pragma omp for schedule (static, chunk)
-		for (i=0; i<BUFFSIZE; i++)
-			for (j=0; j<BUFFSIZE; j++)
+		for (i=0; i<buffSize; i++)
+			for (j=0; j<buffSize; j++)
 				b[i][j]= 1.0;
 		#pragma omp for schedule (static, chunk)
-		for (i=0; i<BUFFSIZE; i++)
-			for (j=0; j<BUFFSIZE; j++)
+		for (i=0; i<buffSize; i++)
+			for (j=0; j<buffSize; j++)
 				c[i][j]= 0;
 
 		/*** Do matrix multiply sharing iterations on outer loop ***/
 		/*** Display who does which iterations for demonstration purposes ***/
 		printf("Thread %d starting matrix multiply...\n",tid);
 		#pragma omp for schedule (static, chunk)
-		for (i=0; i<BUFFSIZE; i++)
+		for (i=0; i<buffSize; i++)
 		{
 			printf("Thread=%d did row=%d\n",tid,i);
-			for(j=0; j<BUFFSIZE; j++)
-                for (k=0; k<BUFFSIZE; k++)
+			for(j=0; j<buffSize; j++)
+                for (k=0; k<buffSize; k++)
                     c[i][j] += a[i][k] * b[k][j];
 		}
 	}   /*** End of parallel region ***/
@@ -77,7 +89,7 @@ int main(int argc, char *argv[])
 	/*** Print results ***/
 	printf("******************************************************\n");
 	printf("Back from multiply in %f seconds\n", dwalltime() - timetick);
-	printf("Result Matrix:\n");
+	printf("Example Matrix:\n");
 	for (i=0; i<5; i++)
 	{
 		for (j=0; j<5; j++)
